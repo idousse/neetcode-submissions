e89bf85b77bb4154769276828c6649e6375ca02f
Ioann <ioanndousse@gmail.com>
2026-06-20 12:21:58 +0200

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        ROWS, COLS = len(board), len(board[0])
+
+        def capture(r,c):
+            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c] != "O"):
+                return
+
+            board[r][c] = "#"
+
+            capture(r-1,c); capture(r+1,c); capture(r,c-1); capture(r, c+1)
+
+        
+        # 1. (DFS) Capture unsurrounded regions (O -> #)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (board[r][c] == "O" and (r in [0,ROWS-1] or c in [0, COLS-1])):
+                    capture(r,c)
+
+        #2. Capture surrounded regions (O -> X)
+        #3. Uncapture unsurrounded regions (# -> O)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "#":
+                    board[r][c] = "O"
+
+                
